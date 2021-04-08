005bdf0dd79774a25120b4de01d9a6edd7c97689
ShinyBPrincy <shinyprincy.jbm@gmail.com>
2021-04-09 01:58:36 +0530

    file added

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,29 @@
+package org.add;
+
+public class GreensTech {
+	
+	public void greensOmr(){
+System.out.println("Area name : OMR");
+}
+	public void greensAdayar(){
+		System.out.println("Area name : Adayar");
+	}
+	public void greensTambaram(){
+		System.out.println("Area name : Tambaram");
+	}
+	public void greensVelacherry(){
+		System.out.println("Area name : Velacherry");
+	}
+	public void greensAnnaNagar(){
+		System.out.println("Area name : AnnaNagar");
+	}
+			
+	public static void main(String[] args) {
+		GreensTech v=new GreensTech();
+		v.greensOmr();
+		v.greensAdayar();
+		v.greensTambaram();
+		v.greensVelacherry();
+		v.greensAnnaNagar();
+	}
+}
